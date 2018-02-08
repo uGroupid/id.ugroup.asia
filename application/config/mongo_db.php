88d58a4e2cd943dcbe480b465543bcd546920e71
handesk <handeskim@users.noreply.github.com>
2018-02-09 06:06:50 +0700

    add handesk

--- application/config/mongo_db.php
+++ application/config/mongo_db.php
@@ -1,33 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/*
-| -------------------------------------------------------------------------
-| DATABASE CONNECTIVITY SETTINGS
-| -------------------------------------------------------------------------
-| This file will contain the settings needed to access your Mongo database.
-|
-|
-| ------------------------------------------------------------------------
-| EXPLANATION OF VARIABLES
-| ------------------------------------------------------------------------
-|
-|	['hostname'] The hostname of your database server.
-|	['username'] The username used to connect to the database
-|	['password'] The password used to connect to the database
-|	['database'] The name of the database you want to connect to
-|	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
-|	['write_concerns'] Default is 1: acknowledge write operations.  ref(http://php.net/manual/en/mongo.writeconcerns.php)
-|	['journal'] Default is TRUE : journal flushed to disk. ref(http://php.net/manual/en/mongo.writeconcerns.php)
-|	['read_preference'] Set the read preference for this connection. ref (http://php.net/manual/en/mongoclient.setreadpreference.php)
-|	['read_preference_tags'] Set the read preference for this connection.  ref (http://php.net/manual/en/mongoclient.setreadpreference.php)
-|
-| The $config['mongo_db']['active'] variable lets you choose which connection group to
-| make active.  By default there is only one group (the 'default' group).
-|
-*/
 
 $config['mongo_db']['active'] = 'ugroup';
 
-$config['mongo_db']['ugroup']['no_auth'] = FALSE;
+$config['mongo_db']['ugroup']['no_auth'] = TRUE;
 $config['mongo_db']['ugroup']['hostname'] = 'localhost';
 $config['mongo_db']['ugroup']['port'] = '27017';
 $config['mongo_db']['ugroup']['username'] = 'ureg';
